9949b07513c32bb62e8d3e9e6299889ef7a51d63
Vitor-Melo <vitorpmelo20@gmail.com>
2021-02-28 12:26:43 -0300

    Estilo do carrinho

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const vm = new Vue({
     produtos: [],
     produto: false,
     carrinho: [],
+    carrinhoAtivo: false,
     messagemAlerta: 'Mensagem alerta',
     alertaAtivo: false,
   },
@@ -46,6 +47,9 @@ const vm = new Vue({
     },
     fecharModal({ target, currentTarget }) {
       if (target === currentTarget) this.produto = false;
+    },
+    clickForaCarrinho({ target, currentTarget }) {
+      if (target === currentTarget) this.carrinhoAtivo = false;
     },
     adicionarItem() {
       this.produto.estoque--;
